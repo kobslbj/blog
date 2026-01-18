49b6e7ba63a81a4eb23062cc2638e7fbfbe2fcd1
JustinLi <kobs666666@gmail.com>
2026-01-19 03:35:10 +0800

    feat: Update personal introduction and add freelance availability with contact information

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,19 @@ export default function Page() {
 				Who am I ?
 			</h1>
 			<p className="mb-4">
-				{`I'm Justin. I'm building a startup. I work on products because they’re interesting, and sometimes they make the world a little better.`}
+				{`I'm Justin. I'm building a startup. I work on products because they're interesting, and sometimes they make the world a little better.`}
+			</p>
+
+			<p className="mb-4">
+				{`I'm currently open for freelance projects. If you have any needs, feel free to reach out. I'm also open to startup collaborations or just chatting about ideas — welcome to connect!`}
+			</p>
+			<p className="mb-4">
+				<a
+					href="mailto:kobs666666@gmail.com"
+					className="underline hover:opacity-70"
+				>
+					kobs666666@gmail.com
+				</a>
 			</p>
 
 			<div className="my-8">
